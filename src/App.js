046df5020b3c1afee2fa64125e414ffd49156045
Zulanas <zachary.zulanas@intel.com>
2019-08-12 16:14:26 -0700

    I am bad at animations

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
 import Helmet from 'react-helmet';
 import useForm from 'react-hook-form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamation } from '@fortawesome/free-solid-svg-icons'
 import 'bulma';
-import './App.css'
-import intelIcon from './intel-xxl.png'
+import './App.css';
+import axios from 'axios';
+import {useSpring, animated, interpolate} from 'react-spring';
+import intelIcon from './intel-xxl.png';
 
 function App() {
+
+    //Data value in textarea
+    const [textAreaVal, setVal] = useState('');
+
+    const handleChange = (event) => {
+      console.log(event.target.textAreaVal)
+      setVal(event.target.textAreaVal);
+    };
+  
+    //Data used in form
+    const { register, handleSubmit, errors, formState, reset } = useForm({
+      mode: 'onBlur'
+    });
+    let username = "{enter username}";
+    let password = "{enter password}";
+    const onSubmit = data => { 
+      let itemIDs = data.products.split("\n")
+      let i;
+      for(i = 0; i< itemIDs.length; i++){
+        itemIDs[i] = itemIDs[i].trim();
+      }
+      axios.get('https://e2esm-sandbox.intel.com/api/intrp/aim/searchByDevices/PSMDEV001 ', {
+        auth: {
+          username: username,
+          password: password
+        },
+      })
+      .then(function(response){
+        console.log(response)
+      })
+      .catch(function(response){
+        console.error("Error! Request is bad")
+        console.log(response)
+      })
+     };
+
+
   //Determines animation for search bar
-  const searchStyle = useSpring({ height: 500, fontSize: 40, overflow: 'auto', padding: 30 })
-  const navStyle = { backgroundColor: '#0071C5' }
-
-  //Data value in textarea
-  const [textAreaVal, setVal] = useState('');
-
-  const handleChange = (event) => {
-    console.log(event.target.textAreaVal)
-    setVal(event.target.textAreaVal);
-  };
-
-  //Data used in form
-  const { register, handleSubmit, errors, formState } = useForm({
-    mode: 'onBlur'
+  const [state1, toggle1] = useState(false);
+  const [state2, toggle2] = useState(false);
+  const searchStyle = useSpring({ height: 500, fontSize: 40, overflow: 'auto', padding: state2 ? 40:30});
+  const errStyle = useSpring({x: errors ? 1 : 0})
+  const wwidStyle = useSpring({
+    padding: state1 ? 50:30,
+    transform: errStyle.x
+      .interpolate({
+        range: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+        output: [0, 100, -100, 100, -100, 100, -100, 100, -100, 0]
+      })
+      .interpolate(
+        x => `transformX(${x})`
+      )
   });
-  const onSubmit = data => { console.log(data) };
+  const navStyle = { backgroundColor: '#0071C5' }
 
   return (
     <div>
@@ -35,12 +73,11 @@ function App() {
         </div>
         <div className="navbar-start">
           <div className="navbar-item">
-            <span style={{ color: 'white', fontSize: 30 }}>LabResSystem</span>
+            <span style={{ color: 'white', fontSize: 30 }}>Lab Librarian</span>
           </div>
         </div>
       </nav>
       <div className="container">
-
         <Helmet>
           <style>{'html { background-color:#cce9ff}'}</style>
         </Helmet>
@@ -51,15 +88,22 @@ function App() {
                 WWID:
             </label>
               <div className="control has-icons-right">
-                <input
+                <animated.input
                   className={"input is-large effect-8 " + (!errors.WWID ? "is-primary" : "is-danger")}
                   placeholder="Scan WWID"
                   name="WWID"
+                  style={wwidStyle}
                   ref={register({
                     required: true,
                     validate: (value) => value.length === 8
                   })}
                   onChange={handleChange}
+                  onFocus={() => toggle1(true)}
+                  onBlur={() => {
+                    toggle1(false)
+                    console.log(errors)
+                  }}
+                  autoComplete = "off"
                 />
                 <span className="focus-border">
                   <i></i>
@@ -78,7 +122,7 @@ function App() {
               <div className="control">
                 <animated.textarea
                   type="text"
-                  className="input effect-8"
+                  className="input"
                   name="products"
                   placeholder="Search..."
                   ref={register}
@@ -87,6 +131,8 @@ function App() {
                   rows="10"
                   onChange={handleChange}
                   disabled={errors.WWID || !formState.dirty}
+                  onFocus={() => toggle2(true)}
+                  onBlur={() => toggle2(false)}
                 />
                 <span className="focus-border">
                   <i></i>
@@ -95,7 +141,7 @@ function App() {
             </div>
             <div>
               <input type="submit" className="button is-link" value="Submit" disabled={errors.WWID || !formState.dirty} style={{margin: 5}}/>
-              <input type="submit" className="button is-danger" value="Reset" disabled={errors.WWID || !formState.dirty} style={{margin: 5}}/>
+              <input type="reset" onClick={reset} className="button is-danger" value="Reset" disabled={errors.WWID || !formState.dirty} style={{margin: 5}}/>
             </div>
           </form>
           <h1>{textAreaVal}</h1>
