7a10f3bcbb26d8e0ac818351aad70a2e9d1fc8bd
Zulanas <zachary.zulanas@intel.com>
2019-08-13 14:40:16 -0700

    small changes to title

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ function App() {
       for(i = 0; i< itemIDs.length; i++){
         itemIDs[i] = itemIDs[i].trim();
       }
+      //This is a 
       axios.get('https://e2esm-sandbox.intel.com/api/intrp/aim/searchByDevices/PSMDEV001 ', {
         auth: {
           username: username,
@@ -52,17 +53,7 @@ function App() {
   const [state2, toggle2] = useState(false);
   const searchStyle = useSpring({ height: 500, fontSize: 40, overflow: 'auto', padding: state2 ? 40:30});
   const errStyle = useSpring({x: errors ? 1 : 0})
-  const wwidStyle = useSpring({
-    padding: state1 ? 50:30,
-    transform: errStyle.x
-      .interpolate({
-        range: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-        output: [0, 100, -100, 100, -100, 100, -100, 100, -100, 0]
-      })
-      .interpolate(
-        x => `transformX(${x})`
-      )
-  });
+  const wwidStyle = useSpring({padding: state1 ? 50:30});
   const navStyle = { backgroundColor: '#0071C5' }
 
   return (
@@ -73,11 +64,20 @@ function App() {
         </div>
         <div className="navbar-start">
           <div className="navbar-item">
-            <span style={{ color: 'white', fontSize: 30 }}>Lab Librarian</span>
+            <div>
+              <span style={{ color: 'white', fontSize: 30,}}>Lab Librarian</span><br/>
+              <span style={{ color: 'white', fontSize: 15}}>Inventory Tool</span>
+            </div>
           </div>
         </div>
       </nav>
       <div className="container">
+
+        {/*
+        React Helmet helps add a lot of pre-processed things such as background colors, metadata, etc.
+
+        It's the equivalent of all the junk in the header tags in HTML
+        */}
         <Helmet>
           <style>{'html { background-color:#cce9ff}'}</style>
         </Helmet>
@@ -88,11 +88,25 @@ function App() {
                 WWID:
             </label>
               <div className="control has-icons-right">
+                {/*
+                This is the WWID input box, it has a lot of junk in it but most of that is just for fancy animations 
+                */}
                 <animated.input
                   className={"input is-large effect-8 " + (!errors.WWID ? "is-primary" : "is-danger")}
                   placeholder="Scan WWID"
                   name="WWID"
-                  style={wwidStyle}
+                  //fancy animation junk, check out https://www.react-spring.io to learn about how this works
+                  style={{
+                    transform: errStyle.x
+                      .interpolate({
+                        range: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+                        output: [0, 100, -100, 100, -100, 100, -100, 100, -100, 0]
+                      })
+                      .interpolate(
+                        x => `translateX(${x})`
+                      )
+                  }}
+                  //This is where WWID Validation happens!
                   ref={register({
                     required: true,
                     validate: (value) => value.length === 8
@@ -120,6 +134,9 @@ function App() {
             <div className="field">
               <label className="label">Search:</label>
               <div className="control">
+                {/*
+                This is the Product textarea to check in things
+                 */}
                 <animated.textarea
                   type="text"
                   className="input"
@@ -140,7 +157,7 @@ function App() {
               </div>
             </div>
             <div>
-              <input type="submit" className="button is-link" value="Submit" disabled={errors.WWID || !formState.dirty} style={{margin: 5}}/>
+              <input type="submit" className="button is-link" value="Check-out" disabled={errors.WWID || !formState.dirty} style={{margin: 5}}/>
               <input type="reset" onClick={reset} className="button is-danger" value="Reset" disabled={errors.WWID || !formState.dirty} style={{margin: 5}}/>
             </div>
           </form>
